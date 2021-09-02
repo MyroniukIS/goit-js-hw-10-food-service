680a24b7cc697aec5635733619fa621867ebda20
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-09-02 20:59:56 +0300

    changes in .js

--- src/js/menu-galary.js
+++ src/js/menu-galary.js
@@ -1,2 +1,13 @@
 import data from "../db/menu.json"
-console.log(data);
+// console.log(data);
+import menuTemplate from "../templates/menuTemplate.hbs"
+// console.log(menuTemplate(data));
+const template = menuTemplate(data);
+console.log(template);
+
+const body = document.querySelector("body");
+// console.log(body);
+
+const ul = document.querySelector(".js-menu");
+// console.log(ul);
+ul.insertAdjacentHTML("afterbegin", template);
